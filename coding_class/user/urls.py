bc850f32e1c986986043b122160513c0e451b56e
parsamhb82 <parsa1382mohebian@gmail.com>
2024-11-06 19:51:15 +0330

    :)

--- coding_class/user/urls.py
+++ coding_class/user/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import UserRegisterView, Login, Refresh, ChangePasswordView
+from .views import UserRegisterView, Login, Refresh, ChangePasswordView, UserProfileUpdateView
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view()),
     path('login/', Login.as_view()),
     path('refresh/', Refresh.as_view()),
     path('change-password/', ChangePasswordView.as_view()),
+    path('update-profile/', UserProfileUpdateView.as_view()),
 
 ]
